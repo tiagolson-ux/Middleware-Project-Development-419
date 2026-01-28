f42a21d8c0aedd49881e88d86689012329a78e65
Tia Noel <tiagolson@gmail.com>
2026-01-28 11:16:11 -0500

    connect mongodb to mongoose

--- config/db.js
+++ config/db.js
@@ -1,17 +1,14 @@
 // config/db.js
-// Note to self: this file handles connecting to MongoDB using Mongoose
+// Note to self: connect to MongoDB using Mongoose
 
 const mongoose = require("mongoose");
 
 async function connectDB(mongoUri) {
-  // Note to self: if mongoUri is missing, fail early with a clear message
   if (!mongoUri) {
     throw new Error("MONGO_URI is missing. Check your .env file.");
   }
 
-  // Note to self: connect to the database; if it fails it will throw an error
   await mongoose.connect(mongoUri);
-
   console.log("MongoDB connected");
 }
 
